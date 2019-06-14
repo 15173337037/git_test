33ab0473d7559c49d5ecd3012405bf29e915bf20
zjx <953194894@qq.com>
2019-06-14 16:50:35 +0800

    把克隆下来的代码进行了更新"1

--- git_test1/src/main/java/com/itheima/git/GitTest1.java
+++ git_test1/src/main/java/com/itheima/git/GitTest1.java
@@ -6,5 +6,7 @@ public class GitTest1 {
 		System.out.println("第一次使用提交到本地仓库");
         System.out.println("第一次使用idea提交到本地仓库");
         System.out.println("上传到github");
+
+        System.out.println("把克隆下来的代码进行了更新");
     }
 }
