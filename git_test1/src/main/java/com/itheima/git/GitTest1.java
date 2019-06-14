2d8dd471375148f3df980d2dd1ca30eeb70bf30c
zjx <953194894@qq.com>
2019-06-14 16:37:34 +0800

    上传github

--- git_test1/src/main/java/com/itheima/git/GitTest1.java
+++ git_test1/src/main/java/com/itheima/git/GitTest1.java
@@ -4,5 +4,7 @@ public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("第一次使用idea提交到本地仓库");
 		System.out.println("第一次使用提交到本地仓库");
+        System.out.println("第一次使用idea提交到本地仓库");
+        System.out.println("上传到github");
     }
 }
